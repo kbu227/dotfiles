002655f499ca86a0218789aee9e42231bf5541ca
kbu <kbukowski227@gmail.com>
2025-11-20 12:32:07 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Update & Upgrade"
+sudo apt-get update && sudo apt-get upgrade -y
+
+# check if ansible is installed
+if !command -v ansible >/dev/null 2>&1; then
+sudo apt install -y ansible
+fi
+
+ansible-playbook kbu_playbook.yaml --ask-become-pass
