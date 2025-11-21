6404d041363adc4340ca219112c51463807eca94
kbu <kbukowski227@gmail.com>
2025-11-21 13:25:36 +0100

    fix issue with the condition (typo)

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ echo "Update & Upgrade"
 sudo apt-get update && sudo apt-get upgrade -y
 
 # check if ansible is installed
-if !command -v ansible >/dev/null 2>&1; then
+if ! command -v ansible >/dev/null 2>&1; then
 sudo apt install -y ansible
 fi
 
